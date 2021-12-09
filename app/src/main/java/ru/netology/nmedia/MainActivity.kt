361ff972c2d0f9f9ea772aaa085e7d13f3a215cf
Yakovlev Alexandr <ventry2016@gmail.com>
2021-12-09 13:19:53 +0500

    Homework2 Commit

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("zzz+ ${R.string.str_nmedia}")
+        println("zzz+ ${getString(R.string.str_nmedia)}")
+        Log.d("rntngranaenafe", "${R.string.str_nmedia}")
         setContentView(R.layout.activity_main)
     }
 }
